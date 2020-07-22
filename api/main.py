3b0663c248fd8555aac6eaea196f93a1ab26c923
Darpan Patel <darpanpatel3131@gmail.com>
2020-07-22 10:41:46 -0300

    landing page

--- api/main.py
+++ api/main.py
@@ -3,6 +3,10 @@ import json
 
 app = Flask(__name__, static_folder="../build", static_url_path='/')
 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
 from ping import ping_blueprint
 app.register_blueprint(ping_blueprint)
 
